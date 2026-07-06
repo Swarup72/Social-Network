6fc3e82911b3c11bfbdaa97471289d84581eccde
Aryan Mole <aryanmole@gmail.com>
2026-07-06 20:36:53 +0530

    ENV connected

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import postRoute from './routes/posts.routes.js'
 import userRoute from './routes/user.routes.js'
+dotenv.config();
 
 const app=express()
 
@@ -15,8 +16,8 @@ app.use("/uploads", express.static("uploads"))
 
 
 const start = async()=>{
-    const connectDB = await mongoose.connect("mongodb+srv://aryanmole_db_user:Pass123@socialnetwork.ekgzhqe.mongodb.net/?appName=SocialNetwork")
-
+    const connectDB = await mongoose.connect(process.env.MONGODB_URI)
+     console.log("MongoDB Connected");
 
     app.listen(8000,()=>[
         console.log("server is running")
